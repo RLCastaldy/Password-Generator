83958ae8fd61a7500716c1c361c40a14df87512d
RLCastaldy <rlcastaldy@gmail.com>
2023-11-12 15:39:29 -0600

    Added all funtions for selecting password options. Attempting to make function start over if No is selcted or to generate password if Yes

--- script.js
+++ script.js
@@ -5,15 +5,50 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var criteria1 = prompt ("Number of Characters? (8-128) ");
-  var criteria2 = 
-    
-  if (criteria1>128) {
-    alert("Invalid Number Of Characters");
-    return(null);
-    } else if (criteria1<8) {
-    alert("Invalid Number Of Characters");
-    return(null);
-    }
+  
+    if (criteria1>128) {
+      alert("Invalid Entry");
+      return(null);
+      } else if (criteria1<8) {
+      alert("Invalid Entry");
+      return(null);
+      };
+
+  var criteria2 = prompt ("Include Lowercase Letters? (Y/N)");
+
+    if (criteria2!="Y" && criteria2!="N") {
+      alert("Invalid Entry");
+      return(null);
+    };
+
+    var criteria3 = prompt ("Include Uppercase Letters? (Y/N)");
+
+    if (criteria3!="Y" && criteria3!="N") {
+      alert("Invalid Entry");
+      return(null);
+    };
+
+    var criteria4 = prompt ("Include Numbers? (Y/N)");
+
+    if (criteria4!="Y" && criteria4!="N") {
+      alert("Invalid Entry");
+      return(null);
+    };
+
+    var criteria5 = prompt ("Include Special Characters? (Y/N)");
+
+    if (criteria5!="Y" && criteria5!="N") {
+      alert("Invalid Entry");
+      return(null);
+    };
+
+    var validate = prompt ("You Have Selected: Number of Characters " + criteria1 + ", Lowercase Letters (" + criteria2 + "), Uppercase Letters (" + criteria3 + "), Numbers (" + criteria4 + "), Special Characters (" + criteria5 + ").  Is this correct? (Y/N)");
+
+    //if (validate ="N") {
+      //function generatePassword();
+    //}; else if (validate ="Y"){
+      //function writePassword();
+    //};
 
 }
 
