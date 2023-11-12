b27025931b8806c4bdab82390df453df09b73663
RLCastaldy <rlcastaldy@gmail.com>
2023-11-12 14:40:22 -0600

    Made program quit upon entering wrong number of characters

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+function generatePassword() {
+  var criteria1 = prompt ("Number of Characters? (8-128) ");
+  var criteria2 = 
+    
+  if (criteria1>128) {
+    alert("Invalid Number Of Characters");
+    return(null);
+    } else if (criteria1<8) {
+    alert("Invalid Number Of Characters");
+    return(null);
+    }
+
+}
+
 function writePassword() {
-  prompt ("Number of Characters? (8-128) ")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
