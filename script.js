65469d60fcfcd7bcb23542463d116f77bd3718e2
RLCastaldy <rlcastaldy@gmail.com>
2023-11-12 22:14:17 -0600

    no changes pushing for assistance

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  const lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  const numbers = [0,1,2,3,4,5,6,7,8,9];
-  const special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","|","/","'","<",",",">",".","?"];
+  //const lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  //const upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  //const numbers = [0,1,2,3,4,5,6,7,8,9];
+  //const special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","|","/","'","<",",",">",".","?"];
+  
+  const lowCase = ['abcdefghijklmnopqrstuvwxyz'];
+  const upCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  const numbers = ['0123456789'];
+  const special = ['~`!@#$%^&*()_-+={}[]|/<>.?'];
 
   const lowEnsure = ["j"]
   const upEnsure = ["N"]
