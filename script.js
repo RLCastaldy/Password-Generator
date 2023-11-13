8f34e68fe7621e21663d912c6e221ca20268cc9e
RLCastaldy <rlcastaldy@gmail.com>
2023-11-12 23:08:19 -0600

    Changed from arrays to a confirm function which drastically simplified my code and made my page work properly

--- script.js
+++ script.js
@@ -4,94 +4,46 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 function generatePassword() {
-
-  //const lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  //const upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  //const numbers = [0,1,2,3,4,5,6,7,8,9];
-  //const special = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","|","/","'","<",",",">",".","?"];
   
-  const lowCase = ['abcdefghijklmnopqrstuvwxyz'];
-  const upCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-  const numbers = ['0123456789'];
-  const special = ['~`!@#$%^&*()_-+={}[]|/<>.?'];
+  const lowCase = 'abcdefghijklmnopqrstuvwxyz';
+  const upCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  const special = '~`!@#$%^&*()_-+={}[]|/<>.?';
 
-  const lowEnsure = ["j"]
-  const upEnsure = ["N"]
-  const numberEnsure = [6]
-  const specialEnsure = [">"]
+  const criteria1 = Number(prompt("Number of Characters? (8-128) "))
 
-  var possibleCharacters = [];
-  var guaranteedCharacters = [];
+  if (criteria1 > 128 || criteria1 < 8) {
+    alert("Number of Characters must be between 8 and 128.");
+    return null;
+  }
 
-  var criteria1 = Number(prompt("Number of Characters? (8-128) "));
-  
-    if (criteria1>128) {
-      alert("Number of Characters Cannot be Greater Than 128.");
-      return(null);
-      } else if (criteria1<8) {
-      alert("Number of Characters Cannot be Less Than 8.");
-      return(null);
-      };
+  const criteria2 = confirm("Include Lowercase Letters?")
+  const criteria3 = confirm("Include Uppercase Letters?")
+  const criteria4 = confirm("Include Numbers?")
+  const criteria5 = confirm("Include Special Characters?")
 
-  var up2 = prompt ("Include Lowercase Letters? (Y/N)");
-  var criteria2 = up2.toUpperCase();
+  if (!(criteria2 || criteria3 || criteria4 || criteria5)) {
+    alert("Must Select At Least One Type Of Character.");
+    return null;
+  }
 
-    if (criteria2!="Y" && criteria2!="N") {
-      alert("Must Select Y or N.");
-      return(null); 
-    } else if (criteria2=="Y") {
-      possibleCharacters=possibleCharacters.concat(lowCase,lowEnsure);
-      };
+  const allChars = [lowCase, upCase, numbers, special];
+  var possibleCharacters = "";
 
-    var up3 = prompt ("Include Uppercase Letters? (Y/N)");
-    var criteria3 = up3.toUpperCase();
+  if (criteria2) possibleCharacters += lowCase;
+  if (criteria3) possibleCharacters += upCase;
+  if (criteria4) possibleCharacters += numbers;
+  if (criteria5) possibleCharacters += special;
 
-    if (criteria3!="Y" && criteria3!="N") {
-      alert("Must Select Y or N.");
-      return(null);
-      } else if (criteria3=="Y") {
-        possibleCharacters=possibleCharacters.concat(upCase,upEnsure);
-        };
+  let password = "";
+  for (let i=0;i<criteria1;i++) {
+    const randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+    password += possibleCharacters.charAt(randomIndex);
+  }
 
-    var up4 = prompt ("Include Numbers? (Y/N)");
-    var criteria4 = up4.toUpperCase();
-
-    if (criteria4!="Y" && criteria4!="N") {
-      alert("Must Select Y or N.");
-      return(null);
-    } else if (criteria4=="Y") {
-      possibleCharacters=possibleCharacters.concat(numbers,numberEnsure);
-      };
-
-    var up5 = prompt ("Include Special Characters? (Y/N)");
-    var criteria5 = up5.toUpperCase();
-
-    if (criteria5!="Y" && criteria5!="N") {
-      alert("Must Select Y or N.");
-      return(null);
-    } else if (criteria5=="Y") {
-      possibleCharacters=possibleCharacters.concat(special,specialEnsure);
-      };
-
-    var upV = prompt ("You Have Selected: Number of Characters " + criteria1 + ", Lowercase Letters (" + criteria2 + "), Uppercase Letters (" + criteria3 + "), Numbers (" + criteria4 + "), Special Characters (" + criteria5 + ").  If this is correct and you would like your random password generated, type Y. If this is incorrect and you would like to restart, type N. Press Cancel to quit.");
-    var validate = upV.toUpperCase();
-
-    if (validate=="N") {
-       generatePassword();
-    } else if (validate=="Y" && criteria2=="N" && criteria3=="N" && criteria4=="N" && criteria5=="N") {
-      alert ("Must Select At Least One Type of Character.");
-      return(null);
-    } else if (validate=="Y" || criteria2=="Y" || criteria3=="Y" || criteria4=="Y" || criteria5=="Y") {
-      for(let i=0;i<criteria1;i++) {
-        var count = 0;
-        for (var j=0; j<Math.floor(Math.random()*criteria1);j++) {
-          count++;
-        }
-        console.log(count);
-      }
-  }  
-
+  return password;
 }
+
 
 function writePassword() {
  
